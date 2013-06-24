5b1fdacf0c550460e61cdb9a50eb5c3464b87fff
rebolon <richard.tribes@gmail.com>
2013-06-24 12:38:49 +0000

    mise en place du packaging de l'animator

--- magicCss4MeteorJs.js
+++ magicCss4MeteorJs.js
@@ -1,102 +1,145 @@
 Members = new Meteor.Collection('members');
 
+animatorAutomation = {
+	collectionState: {},
+
+	effectOnAdded: function (selector, item) {
+	  // check to prevent effect when added coz removed has been refused server-side
+	  if (item._removeAnimationPlayed === undefined) {
+            $(selector).removeClass().addClass('magictime swashIn');
+          }
+	},
+
+	effectOnRemoved: function (selector, item, timeout) {
+	  // play animation
+          $(selector).removeClass().addClass('magictime swashOut');
+          // and remove finally the item
+          Meteor.setTimeout(function () {
+            Members.update(item._id, {$set: {_removeAnimationPlayed: true}}, function () {
+              Members.remove(item._id);
+            });
+          }, timeout);
+	},
+
+	effectOnChanged: function (selector, item) {
+	  $(selector).removeClass().addClass('magictime vanishIn');
+	},
+
+	alterTemplate: function (tpl, timeout) {
+	  var self = this;
+	  timeout = timeout || 1000;
+
+	  tpl.rendered = function () {
+console.log('tpl rendered...');
+	    var item = this.data;
+	    switch (self.collectionState[item._id]) {
+	        case 'added':
+		  self.effectOnAdded('#' + item._id, item);
+	          break;
+	        case 'removed':
+		  self.effectOnRemoved('#' + item._id, item, timeout);
+	          break;
+	        case 'changed':
+		  self.effectOnChanged('#' + item._id);
+	          break;
+	        default:
+console.log('behavior not expected');
+	    }
+console.log('tpl item rendered: ', this, self.collectionState[item._id]);
+
+	    // clear status
+	    self.collectionState[item._id] = null;
+	  };
+	},
+
+	alterCursorObserver: function (cursor) {
+	    var self = this;
+	    cursor.observe({
+	      added: function (newDocument) {
+		// default behavior
+	        self.collectionState[newDocument._id] = 'added';
+		// added for a removed next step coz first removed has been refused server-side
+	        if (newDocument._removeAnimationPlayed === false) {
+	          self.collectionState[newDocument._id] = 'removed';
+	        }
+console.log('Members added: ', this, newDocument);
+	      },
+	
+	      removed: function (oldDocument) {
+	        self.collectionState[oldDocument._id] = 'removed';
+console.log('Members removed: ', this, oldDocument);
+	      },
+	
+	      changed: function (newDocument, oldDocument) {
+	        // default behavior
+	        self.collectionstate[newDocument._id] = 'changed';
+		// changed for a removed next step
+	        if (newDocument._removeAnimationPlayed === false) {
+	          self.collectionState[newDocument._id] = 'removed';
+	        }
+console.log('Members changed: ', this, newDocument, oldDocument);
+	      }
+	  });
+	},
+
+	alterCollectionAllow: function (collection) {
+	  // default behavior on insecure mode
+	  if (!collection._validators
+		|| !collection._validators.insert.allow
+		|| collection._validators.insert.allow.length < 1) {
+	    collection.allow({
+		insert: function (userId, document) {
+		  return true;
+		}
+	    });
+	  }
+
+	  if (!collection._validators
+                || !collection._validators.update.allow
+		|| collection._validators.update.allow.length < 1) {
+            collection.allow({
+                update: function (userId, document) {
+                  return true;
+                }
+            });
+          }
+
+	  // behavior for the animation
+	  collection.allow({
+	    remove: function (userId, document) {
+console.log('Members allow remove: ', document._id);
+	        if (document._removeAnimationPlayed === true) {
+	          return true;
+	        } else {
+	          collection.update(document._id, {$set: {_removeAnimationPlayed: false}});
+	          return false;
+	        }
+	    }
+	  });
+	}
+  };
+ 
 if (Meteor.isClient) {
-  MembersCollectionState = {};
+  Meteor.startup(function () {
+
+  });
 
   // Remove authorized only if property removeAnimationPlayed set to true
   // @TODO find a way to store that data ouside the document property
   // @TODO remove animation should also change that property and then recall the remove function
 
   membersList = Members.find({}, {sort: {date: 1}});
- 
-  membersList.observe({
-    added: function (newDocument) {
-	MembersCollectionState[newDocument._id] = 'added';
-	// in fact i pass in added when item has been removed and that remove has been denied by server, so it's added again on client-side
-	if (newDocument.removeAnimationPlayed === false) {
-          MembersCollectionState[newDocument._id] = 'removed';
-        }
-console.log('Members added: ', this, newDocument);
-    },
 
-    removed: function (oldDocument) {
-	MembersCollectionState[oldDocument._id] = 'removed';
-console.log('Members removed: ', this, oldDocument);
-	/*
-	var el = $('#'+oldDocument._id);
-	el.before('<div>ByBye</div>'); // will be removed at the same time as the main template... what am i doing wrong ?
-	*/
-    },
-
-    changed: function (newDocument, oldDocument) {
-	// default behavior
-	MembersCollectionState[newDocument._id] = 'changed';
-	if (newDocument.removeAnimationPlayed === false) {
-	  MembersCollectionState[newDocument._id] = 'removed';
-	}
-console.log('Members changed: ', this, newDocument, oldDocument);
-    }
-  });
-
-  Template.container.inList = function () {
-    return membersList;
-  };
+  animatorAutomation.alterTemplate(Template.item);
+  animatorAutomation.alterCursorObserver(membersList);
 
   Template.item.created = function () {
+//@TODO use created to set a var that will disable animation of first render ?
 console.log('tpl item created: ', this);
   };
 
-  Template.item.destroyed = function () {
-console.log('tpl item destroyed: ', this);
-  };
-
-  Template.item.rendered = function () {
-    var item = this.data;
-    switch (MembersCollectionState[item._id]) {
-	case 'added':
-	  if (item.removeAnimationPlayed === undefined) {
-	    $('#' + item._id).removeClass().addClass('magictime swashIn');
-	  }
-	  break;
-	// ne fonctionne pas car c'est un callback après rendered.
-	// test de déplacement dans le observe.removed, pas mieux
-	// test avec remplacement du DOM via jQuery ($('#{{_id}}').replaceChild(monNouveaudom)): id="{{_id}}" par id="removed_{{_id}}" => Meteor va quand même supprimer le noeud
-	// test en rajoutant un noeud avant via jQuery ($('#{{_id}}').before(monNouveauDom)): Meteor supprime quand même ce noeud qui est pourtant en dehors du noeud géré
-	case 'removed':
-	  // play animation
-	  $('#' + item._id).removeClass().addClass('magictime swashOut');
-	  // and remove finally the item
-	  Meteor.setTimeout(function () {
-		Members.update(item._id, {$set: {removeAnimationPlayed: true}}, function () {
-		  Members.remove(item._id);
-		});
-	  }, 1000);
-	  break;
-	case 'changed':
-	  $('#' + item._id).removeClass().addClass('magictime vanishIn');
-	  break;
-	default:
-    }
-console.log('tpl item rendered: ', this, MembersCollectionState[item._id]);
-
-    // clear status
-    MembersCollectionState[item._id] = null;
-  };
-
   Template.container.inList = function () {
     return membersList;
-  };
-
-  Template.container.created = function () {
-console.log('tpl container created: ', this);
-  };
-
-  Template.container.destroyed = function () {
-console.log('tpl container destroyed: ', this);
-  };
-
-  Template.container.rendered = function () {
-console.log('tpl container rendered: ', this);
   };
 }
 
@@ -105,24 +148,5 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 
-    Members.allow({
-    insert: function (userId, document) {
-console.log('Members allow insert: ', document._id);
-        return true;
-    },
-
-    remove: function (userId, document) {
-console.log('Members allow remove: ', document._id);
-	if (document.removeAnimationPlayed === true) {
-          return true;
-	} else {
-	  Members.update(document._id, {$set: {removeAnimationPlayed: false}});
-	  return false;
-	}
-    },
-
-    update: function (userId, doc, fields, modifier) {
-        return true;
-    }
-  });
+  animatorAutomation.alterCollectionAllow(Members);
 }
